03ae15631f94b692bbdf46f07f244d9900a387d1
Likitha <97089754+likitha1811@users.noreply.github.com>
2022-02-09 05:19:50 +0000

    A

--- p1original.c
+++ p1original.c
@@ -1,2 +1,23 @@
-
+#include <stdio.h>
+void input(float *base, float *height)
+{
+  printf("Enter length of base and height of triangle:\n");
+  scanf("%f%f",base,height);
 }
+void find_area(float base , float height, float *area)
+{
+  *area = (base*height)/0.5 ;
+}
+void output(float base, float height, float area)
+{
+  printf("Area of Triangle is (%f*%f)/0.5 = %f\n ", area,base,height);
+}
+float main()
+{
+  float base,height, area=0;
+  input(&height,&base);
+  find_area(height,base,&area);
+  output(base,height,area);
+  return 0; 
+}
+
